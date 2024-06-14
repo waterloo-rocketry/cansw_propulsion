4c385e495b1483ce6cb7e04a51eaa5224bac9b09
Pranav Mahabal <95592003+Pdada1@users.noreply.github.com>
2024-06-13 20:50:00 -0400

    IOExpander Driver File

---
+++ IOExpanderDriver.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   IOExpanderDriver.c
+ * Author: Pranav Mahabal
+ *
+ * Created on June 13, 2024, 8:47 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
---
+++ IOExpanderDriver.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   IOExpanderDriver.h
+ * Author: Pranav Mahabal
+ *
+ * Created on June 13, 2024, 8:47 PM
+ */
+
+#ifndef IOEXPANDERDRIVER_H
+#define	IOEXPANDERDRIVER_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* IOEXPANDERDRIVER_H */
+
